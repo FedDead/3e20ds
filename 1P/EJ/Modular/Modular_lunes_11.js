f60025b3d2ee920620fd8ef83176bce68084e93f
Francisco Fidel Castro Barreto <61883040+FedDead@users.noreply.github.com>
2020-11-17 12:13:46 -0600

    Update Modular_lunes_11.js

--- 1P/EJ/Modular/Modular_lunes_11.js
+++ 1P/EJ/Modular/Modular_lunes_11.js
@@ -1,22 +1,29 @@
 let day=new Date();
 let mes=new Date();
+let diasemana=new Date();
+var semana=new Array('Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado');
 const mod=11;
 day=day.getDate();
 mes=mes.getMonth()+1;
 console.log("El dia actual es "+day);
 day=day+mod;
+diasemana=day;
+diasemana=diasemana%7-1;
 if(mes==0 || mes==1 || mes==3 || mes==5 ||mes==5 || mes==7||mes==8||mes==10){
     mes=31;
 }
 else if(mes==2 || mes==4 || mes==6 || mes==9 ||mes==11){
     mes=30;
 }
+for(let i=0;i<=6;i++){
+    if(diasemana==i){
+        diasemana=semana[i];
+    }
+}
 if(day>mes){
     day=day-mes; 
-    console.log("El dia despues de 11 dias es "+day);
+    console.log("El dia despues de 11 dias es "+day+" "+diasemana);
 }
 else if(day<mes){ 
-    console.log("El dia hace de 11 dias es "+day);
+    console.log("El dia despues de 11 dias es "+day+" "+diasemana);
 }
-
-
